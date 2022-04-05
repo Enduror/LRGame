4bac48773d407e7d07ef951357a91381c1898b86
GitLuc36 <zech.lucas@googlemail.com>
2022-04-05 22:23:30 +0200

    Update ServingPosition.cs
    Reworked Serve values and initial posion.
    Code clean up

--- LRGame/Assets/ServingPosition.cs
+++ LRGame/Assets/ServingPosition.cs
@@ -12,56 +12,47 @@ public class ServingPosition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float Xstart = Xrandom();
-        Vector3 ServingPlayerPosition = new Vector3(Xstart, 1f, 10f);
-        transform.position = ServingPlayerPosition;
-        InitializeBall(ServingPlayerPosition);
-        
+        SetServePosition();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        // input to position serving player
+        // input to set the serving position of player and ball
         if (Input.GetKeyDown("p")) 
         {
-            float Xstart = Xrandom();
-            Vector3 ServingPlayerPosition = new Vector3(Xstart, 1f, 10f);
-            transform.position = ServingPlayerPosition;
-            InitializeBall(ServingPlayerPosition);
+            SetServePosition();
         }
 
         // input to execute tennis serve
         if (Input.GetKeyDown("o"))
         {
             // inital flight direction
-            GameObject.Find("BallWithTexture(Clone)").GetComponent<Transform>().position = transform.position + new Vector3(0, 4, 0);
+            GameObject.Find("BallWithTexture(Clone)").GetComponent<Transform>().position = transform.position + new Vector3(0, 2, 0);
             GameObject.Find("BallWithTexture(Clone)").GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            float YrandomStartVelocity = Random.Range(-4f, 0f);
-            float ZrandomStartVelocity = Random.Range(22f, 30f);
+            float YrandomStartVelocity = Random.Range(2.5f, 4.5f);
+            float ZrandomStartVelocity = Random.Range(23f, 27f);
             GameObject.Find("BallWithTexture(Clone)").GetComponent<Rigidbody>().velocity = new Vector3(0f, YrandomStartVelocity, -ZrandomStartVelocity);
 
             // inital angularVelocity
-
             float XrandomStartAngularVelocity = Random.Range(TennisHitSkill, 2 * TennisHitSkill);
             float YrandomStartAngularVelocity = Random.Range(TennisHitSkill / 2, TennisHitSkill);
             GameObject.Find("BallWithTexture(Clone)").GetComponent<Rigidbody>().angularVelocity = new Vector3(-XrandomStartAngularVelocity, -YrandomStartAngularVelocity, 0f);
             FloatServe = false;
 
         }
+
         // input to execute float serve
         if (Input.GetKeyDown("i"))
         {
             FloatServe = true;
             // inital flight direction
-            GameObject.Find("BallWithTexture(Clone)").GetComponent<Transform>().position = transform.position + new Vector3(0, 4, 0);
+            GameObject.Find("BallWithTexture(Clone)").GetComponent<Transform>().position = transform.position + new Vector3(0, 2, 0);
             GameObject.Find("BallWithTexture(Clone)").GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            // GameObject.Find("BallWithTexture(Clone)").GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-            float YrandomStartVelocity = Random.Range(0f, 3f);
-            float ZrandomStartVelocity = Random.Range(17f, 22f);
+            float YrandomStartVelocity = Random.Range(3f, 5f);
+            float ZrandomStartVelocity = Random.Range(20f, 23f);
             GameObject.Find("BallWithTexture(Clone)").GetComponent<Rigidbody>().velocity = new Vector3(0f, YrandomStartVelocity, -ZrandomStartVelocity);
-
             // inital angularVelocity
             float XrandomStartAngularVelocity = Random.Range(-FloatHitSkill, FloatHitSkill);
             float YrandomStartAngularVelocity = Random.Range(-FloatHitSkill, FloatHitSkill);
@@ -87,20 +78,17 @@ public class ServingPosition : MonoBehaviour
         }
     }
 
-    // Create random position of the serving opponent player
-    float Xrandom()
-    {
-        float XrandomStart = (float)Random.Range(-4, 4);
-        return XrandomStart;
-    }
-
-    // initialize ball above serving opponent player
-    void InitializeBall(Vector3 ServingPlayerPosition)
+    // Set position of serving opponent player and initialize ball above
+    void SetServePosition()
     {
         Destroy(GameObject.Find("BallWithTexture(Clone)"));
-        Vector3 ServingBallPosition = ServingPlayerPosition + new Vector3(0, 4, 0);
+        // Player position
+        float XrandomStart = (float)Random.Range(-4, 4);
+        Vector3 ServingPlayerPosition = new Vector3(XrandomStart, 1f, 10f);
+        transform.position = ServingPlayerPosition;
+        // Ball initialized and placed above player
+        Vector3 ServingBallPosition = ServingPlayerPosition + new Vector3(0, 2, 0);
         Instantiate(Ball, ServingBallPosition, Quaternion.identity);
-        // Ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         Ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
     }
 
